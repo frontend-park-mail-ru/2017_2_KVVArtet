a8f9e488c6f2b2de06dfdf5be1307a5d26683c46
WorldVirus <metallika956@gmail.com>
2017-10-27 16:27:47 +0300

    game realization

--- public/routing/index.js
+++
@@ -1,7 +0,0 @@
-let postRegistration = require( './signup/post');
-let post = require('./signin/post');
-
-module.export =  (app) => {
-    app.post('/signup', postRegistration);
-    app.post('/signin', post);
-};
